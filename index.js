54d8b6ec187365615ad000d12fe27c620fba782a
Mehki Joseph <109314751+mehkij@users.noreply.github.com>
2023-11-30 01:22:28 -0500

    Add email error handling

---
+++ index.js
@@ -0,0 +1,16 @@
+const form = document.querySelector("form");
+const email = document.getElementById("mail");
+const zipCode = document.getElementById("zip");
+const emailError = document.querySelector("#mail + span.error");
+const zipCodeError = document.querySelector("#zip + span.error");
+
+function showEmailError() {
+  if (email.validitiy.valueMissing) {
+    emailError.textContent = "You need to enter an email address.";
+  } else if (email.validitiy.typeMismatch) {
+    emailError.textContent = "Entered value needs to be an email address."
+  }
+
+  emailError.className = "error active";
+};
+
